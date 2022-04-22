d982e1761b702d7a8c285a96a2529d31e29aa36c
Aaron Ackbarali <Aaron.Ackbarali@gmail.com>
2022-04-22 03:55:55 -0400

    unused #define

--- src/SPI/EPD/Fonts/fonts.h
+++ src/SPI/EPD/Fonts/fonts.h
@@ -2,11 +2,6 @@
 #ifndef __FONTS_H
 #define __FONTS_H
 
-/* Max size of bitmap will based on a font24 (17x24) */
-#define MAX_HEIGHT_FONT         24
-#define MAX_WIDTH_FONT          17
-#define OFFSET_BITMAP           54
-
 #include <stdint.h>
 
 struct sFONT {
